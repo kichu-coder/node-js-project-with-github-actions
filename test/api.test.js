485a1e734e17815cdeb669c6339172a17c1ca68c
kichu-coder <learningkish@gmail.com>
2025-03-25 14:38:21 +0530

    changing the import type

--- test/api.test.js
+++ test/api.test.js
@@ -1,5 +1,5 @@
-import request from "supertest";
-import app from '../src/app';
+const request = require('supertest');
+const app = require('../src/app');
 
 describe('API Endpoints', () => {
   it('should return API status', async () => {
